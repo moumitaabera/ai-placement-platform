e01dd054ff0ccef4821cb2dfb6c2d904272f9445
Moumita Bera <beramoumita036@gmail.com>
2026-09-09 18:43:11 +0530

    Fix Express error middleware signature

--- apps/api/eslint.config.js
+++ apps/api/eslint.config.js
@@ -12,4 +12,14 @@ export default tseslint.config(
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
+  {
+  rules: {
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        argsIgnorePattern: "^_",
+      },
+    ],
+  },
+},
 );
